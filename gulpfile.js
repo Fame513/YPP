04069a2d25e74d315df493f9c98adfff287de1c3
Fame <SlavaNek@yandex.ru>
2018-02-26 22:33:22 +0200

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,12 @@ const gulp = require('gulp'),
   typescript = require('typescript'),
   ts = require('gulp-typescript'),
   tsProject = ts.createProject('tsconfig.json', {typescript: typescript}),
+  tsServer = ts.createProject('server/tsconfig.json', {typescript: typescript}),
   logger = require('gulp-logger'),
   browserify = require("browserify"),
   rename = require('gulp-rename'),
-  del = require('del');
-;
+  del = require('del')
+  gulpCopy = require('gulp-copy');
 
 const popupSrc = 'popup/**/*.tsx';
 const tempDir = './.tmp';
@@ -27,6 +28,71 @@ gulp.task('default', function () {
     }))
     .pipe(gulp.dest('popup'))
     
+});
+
+gulp.task('build', ['build:plugin', 'build:server']);
+
+gulp.task('build:plugin', ['build:plugin:popup', 'build:plugin:background', 'copy']);
+
+gulp.task('build:plugin:popup', function () {
+  return gulp.src(popupSrc)
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest(tempDir))
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(rename((path) => {
+      path.basename = "popup";
+      if (path.extname === '.map') {
+        path.extname = '.js.map'
+      }
+    }))
+    .pipe(gulp.dest('dist/plugin/popup'))
+});
+
+gulp.task('copy', ['copy:manifest', 'copy:scripts', 'copy:options', 'copy:content', 'copy:popup.html']);
+
+gulp.task('copy:manifest', function () {
+  gulp
+    .src('manifest.json')
+    .pipe(gulpCopy('dist/plugin'))
+});
+
+gulp.task('copy:scripts', function () {
+  gulp
+    .src('scripts/**')
+    .pipe(gulpCopy('dist/plugin'))
+});
+
+gulp.task('copy:options', function () {
+  gulp
+    .src('options/**')
+    .pipe(gulpCopy('dist/plugin'))
+});
+
+gulp.task('copy:content', function () {
+  gulp
+    .src('content/**')
+    .pipe(gulpCopy('dist/plugin'))
+});
+
+gulp.task('copy:popup.html', function () {
+  gulp
+    .src('popup/popup.html')
+    .pipe(gulpCopy('dist/plugin'))
+});
+
+gulp.task('build:plugin:background', ['build:plugin:popup'], function () {
+  return gulp.src('background/background.ts')
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest('dist/plugin/background'))
+});
+
+gulp.task('build:server', function () {
+  return gulp.src('server/index.ts')
+    .pipe(tsServer())
+    .js
+    .pipe(gulp.dest('dist/server'))
 });
 
 gulp.task('watcher',function(){
